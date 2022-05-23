aeac1331ac2ee05b15fea5e5f841a0ea1773c89f
vadim1orlinoe <106035837+vadim1orlinoe@users.noreply.github.com>
2022-05-23 20:23:09 +0300

    Add files via upload

---
+++ bios for arduino.py
@@ -0,0 +1,20 @@
+import time
+
+
+
+while True:
+	y = 'load os'
+	y2 = 'exit'
+	print("exit")
+	print("load os")
+	c = input("exit and load os:")
+	if c == y:
+		j = input("command:")
+
+
+
+		
+
+
+
+
